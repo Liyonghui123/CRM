43e21547fd27e58df6f4b17a054f19e99d584b74
lyh <837465803@qq.com>
2020-03-06 21:47:40 +0800

    建立项目

---
+++ src/main/java/com/douyu/CmsApplication.java
@@ -0,0 +1,16 @@
+package com.douyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: Dangerous
+ * @time: 2020/3/6 21:37
+ */
+@SpringBootApplication
+public class CmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class,args);
+    }
+}
---
+++ src/main/java/com/douyu/controller/ClientController.java
@@ -0,0 +1,18 @@
+package com.douyu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: Dangerous
+ * @time: 2020/3/6 21:38
+ */
+@RestController
+public class ClientController {
+    @RequestMapping(value = "index",method = RequestMethod.GET)
+    public String index(){
+        return "hello CRMs";
+    }
+}
